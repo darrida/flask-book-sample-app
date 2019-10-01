95fd10af326c5a0ea9e453a13d95deced267811c
darrida <darridapy@gmail.com>
2019-09-30 21:45:35 -0500

    ch8 upgrade db for auth p135

--- flasky/tests/test_user_model.py
+++ flasky/tests/test_user_model.py
@@ -5,7 +5,7 @@ from app.models import User
 class UserModelTestCase(unittest.TestCase):
     def test_password_setter(self):
         u = User(password = 'cat')
-        self.assertTrue(u.password_has is not None)
+        self.assertTrue(u.password_hash is not None)
         
     def test_no_password_getter(self):
         u = User(password = 'cat')

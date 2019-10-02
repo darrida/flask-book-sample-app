ffdbf35bdc9a01b8aeac596b2daeefbf80ea7f05
darrida <darridapy@gmail.com>
2019-10-01 22:26:33 -0500

    c8 Sending Confirmation Emails p142

--- flasky/app/auth/forms.py
+++ flasky/app/auth/forms.py
@@ -13,6 +13,8 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login In')
 
 class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
     username = StringField('Username', validators=[
         DataRequired(), Length(1, 64),
         Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,

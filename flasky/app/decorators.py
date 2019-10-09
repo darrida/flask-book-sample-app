2693d8e738ed489c06d94058b43e8a3bba2fab8f
darrida <darridapy@gmail.com>
2019-10-09 00:25:27 -0500

    ch8 role-verification p154

--- flasky/app/decorators.py
+++ flasky/app/decorators.py
@@ -4,9 +4,8 @@ from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Role, Permission
 
-
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-migrade = Migrate(app, db)
+migrate = Migrate(app, db)
 
 
 @app.shell_context_processor

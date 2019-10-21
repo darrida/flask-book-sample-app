f4127bfcfc882e2c68fdea560186f7e6712e69f2
darrida <darridapy@gmail.com>
2019-10-21 15:08:24 -0500

    ch10 User Profile Page p161

--- flasky/app/models.py
+++ flasky/app/models.py
@@ -3,6 +3,7 @@ from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
+from datetime import datetime
 
 
 class Permission:
@@ -75,6 +76,11 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -158,6 +164,12 @@ class User(UserMixin, db.Model):
     
     def __repr__(self):
         return '<User %r>' % self.username
+    
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+        db.session.commit()
+        
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):

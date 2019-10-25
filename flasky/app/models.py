21622a126161b9ad2310f1c4e13a6a6994f201e1
darrida <darridapy@gmail.com>
2019-10-25 00:32:58 -0500

    ch12 Followers finished

--- flasky/app/models.py
+++ flasky/app/models.py
@@ -134,7 +134,9 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(default=True).first()
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = self.gravatar_hash()
-             
+        self.follow(self)
+    
+         
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -210,7 +212,8 @@ class User(UserMixin, db.Model):
             url = 'https://secure.gravatar.com/avatar'
         else:
             url = 'http://secure.gravatar.com/avatar'
-        hash = hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        #hash = hashlib.md5(self.email.lower().encode('utf-8')).hexdigest()
+        hash= self.avatar_hash or self.gravatar_hash()
         return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
                 url=url, hash=hash, size=size, default=default, rating=rating)
     
@@ -254,6 +257,14 @@ class User(UserMixin, db.Model):
     def followed_posts(self):
         return Post.query.join(Follow, Follow.followed_id == Post.author_id)\
             .filter(Follow.follower_id == self.id)
+            
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
 
 
 class AnonymousUser(AnonymousUserMixin):
